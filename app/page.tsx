ed8c8a6bf59289d9ad5c47496afb338500eb3f67
Artemy-sweden <ghosteagle2020@mail.ru>
2024-12-22 23:20:57 +0300

    2-hours

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
-import { Button } from "@/components/ui/button";
+import {
+  Container,
+  Filters,
+  ProductCard,
+  ProductsGroupList,
+  Title,
+  TopBar,
+} from "@/components/shared";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="outline">click</Button>
-    </div>
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[80px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Пиццы"
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер",
+                    imageUrl: "...",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
